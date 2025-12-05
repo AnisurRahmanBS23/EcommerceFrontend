4204979def82eed82171014ad85ed240387cc389
Anisur Rahman <anissarker301@gmail.com>
2025-12-05 16:32:58 +0600

    feat: Add production environment configuration with API endpoints.

--- ecommerce-app-frontend/src/environments/environment.prod.ts
+++ ecommerce-app-frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://ecommerceapp-production-a915.up.railway.app',
-  authApi: 'https://ecommerceapp-production-a915.up.railway.app/auth/api',
-  productApi: 'https://ecommerceapp-production-a915.up.railway.app/products/api',
-  orderApi: 'https://ecommerceapp-production-a915.up.railway.app/orders/api',
+  authApi: 'https://ecommerceapp-production-a915.up.railway.app',
+  productApi: 'https://ecommerceapp-production-a915.up.railway.app',
+  orderApi: 'https://ecommerceapp-production-a915.up.railway.app',
   version: '1.0.0'
 };
